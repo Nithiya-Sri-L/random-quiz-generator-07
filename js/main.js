1ed0e25ff7bc9d64607d7fd6cd85487ece296a71
nithiya <nithiyasrisri3@gmail.com>
2022-11-27 16:28:19 +0530

    base file

---
+++ js/main.js
@@ -0,0 +1,25 @@
+var questionBank = [];
+
+
+questionBank.push(problem071());
+questionBank.push(problem072());
+questionBank.push(problem073());
+questionBank.push(problem074());
+questionBank.push(problem075());
+questionBank.push(problem076());
+questionBank.push(problem077());
+questionBank.push(problem078());
+questionBank.push(problem079());
+questionBank.push(problem0710());
+questionBank.push(problem0711());
+questionBank.push(problem0712());
+questionBank.push(problem0713());
+questionBank.push(problem0714());
+questionBank.push(problem0715());
+questionBank.push(problem0716());
+questionBank.push(problem0717());
+questionBank.push(problem0718());
+questionBank.push(problem0719());
+questionBank.push(problem0720());
+
+console.log(questionBank);
